959c6b422ade172ebd9587df39fe88885b9c01ed
hbk <3243430237@qq.com>
2020-02-06 18:08:52 +0800

    first commit

---
+++ script/config.js
@@ -0,0 +1,76 @@
+var config = {
+    "debug": true, // 输出日志信息到控制台
+    "theme": "default dark", // 引用colorscheme中的字段
+    "help_search": true, // 为true时使用定义好的搜索参数
+
+    "engine": {//{{{
+        "index": 2, // 使用搜索引擎列表中的哪个搜索引擎，1 为第一个
+        "trans_index": 1, // 使用翻译列表中的哪个翻译引擎，1 为第一个
+        "translate": [
+            // title  引擎名字
+            // url:   引擎地址, 使用${word}标记翻译词
+            {
+                "title": "Google翻译",
+                "url": "https://translate.google.cn/#view=home&op=translate&sl=auto&tl=zh-CN&text=${word}"
+            }
+        ],
+        "list": [
+            // title: 搜索引擎名字
+            // url:   搜索引擎地址, 使用${word}标记搜索词
+            // image: 搜索引擎图标, 目前没用, 以后可能会用上
+            {
+                "title": "秘迹",
+                "url": "https://mijisou.com/?q=${word}",
+                "image": "https://mijisou.com/favicon.ico"
+            },
+            {
+                "title": "Bing",
+                "url": "https://cn.bing.com/search?q=${word}",
+                "image": "https://cn.bing.com/favicon.ico"
+            }
+        ]
+    },//}}}
+
+    "website": [//{{{
+        // title: 网站名字
+        // url:   网站链接
+        // image: 网站图标
+        {
+            "title": "哔哩哔哩",
+            "url": "https://bilibili.com",
+            "image": "https://bilibili.com/favicon.ico"
+        },
+        {
+            "title": "知乎",
+            "url": "https://www.zhihu.com",
+            "image": "https://www.zhihu.com/favicon.ico"
+        },
+        {
+            "title": "GitHub",
+            "url": "https://github.com",
+            "image": "https://github.com/favicon.ico"
+        },
+        {
+            "title": "谷歌翻译",
+            "url": "https://translate.google.cn/",
+            "image": "https://translate.google.cn/favicon.ico"
+        },
+        {
+            "title": "腾讯视频",
+            "url": "https://v.qq.com/",
+            "image": "http://img.25pp.com/uploadfile/app/icon/20161130/1480498815467743.jpg"
+        }
+    ],//}}}
+
+    "colorscheme": {
+        // theme name, theme file name
+        "default dark": "default_dark",
+        "default light": "default_light"
+    },
+
+    // The function will run in window.onload
+    "onload": function () {
+        // document.getElementsByClassName("title")[0].innerText = "小爱同学";
+        console.log("hello");
+    }
+}
---
+++ script/main.js
@@ -0,0 +1,191 @@
+var engine_now;
+var translate_engine_now;
+
+window.onload = function () {
+    
+    config.onload();
+
+    init();
+
+    // load theme
+    var themeFile = config.colorscheme[this.config.theme];
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    if (themeFile == undefined) {
+        this.console.log("Theme not found: [" + config.theme + "], Will use to [default_dark]");
+        link.href = "./theme/default_dark.css";
+    } else {
+        link.href = "./theme/" + themeFile + ".css";
+    }
+
+    document.getElementsByTagName("head")[0].appendChild(link);
+
+
+
+    // 加载搜索引擎logo
+    // var search_image = document.getElementsByClassName("search-image")[0];
+    // var image_url = search_engine_image[search_engine_index - 1];
+    // search_image.setAttribute("src", image_url);
+    // var search_btn  = document.getElementsByClassName("search-btn")[0];
+    // search_btn.value = "";
+    // search_btn.style.background = "url(\"" + image_url + "\")";
+
+    // load engine name
+    var search_btn = document.getElementsByClassName("btn search")[0];
+    search_btn.value = engine_now.title;
+    var trans_btn = document.getElementsByClassName("btn trans")[0];
+    trans_btn.value = translate_engine_now.title;
+
+    // load website.
+    config.website.forEach(page => {
+        if (page.image.startsWith(".")) {
+            var perfix = page.url;
+            if (page.url.endsWith("/")) {
+                perfix = page.url.substring(0, page.url.length - 1);
+            }
+
+            var endfix = page.image.substring(1, page.image.length);
+            addWebsite(page.title, page.url, perfix + endfix);
+        } else {
+            addWebsite(page.title, page.url, page.image);
+        }
+    });
+};
+
+
+function init() {
+    engine_now = config.engine.list[config.engine.index - 1];
+    translate_engine_now = config.engine.translate[config.engine.trans_index - 1];
+
+    // 监听按键，enter为搜索，alt+enter为翻译
+    document.onkeydown = function (e) {
+        if (!transEnter(e)) {
+            searchEnter();
+        }
+    }
+}
+
+
+
+/**
+ * 搜索
+ */
+function search() {
+    // 获取输入文本
+    var text = document.getElementsByClassName("input")[0].value;
+    if (text != "") {
+        // 是否要添加搜索参数
+        if (config.help_search && text.indexOf(" ") > 0) {
+            var lang = text.substring(0, text.indexOf(" "));
+            var inlist = [
+                "java",
+                "python",
+                "c",
+                "c++",
+                "js",
+                "javascript",
+                "html",
+                "css",
+                "pycharm",
+                "vim",
+                "shell",
+                "linux",
+                "Linux",
+                "win",
+                "win10",
+            ];
+
+            inlist.forEach(value => {
+                if (value == lang) {
+                    text = text.replace(lang, "\"" + lang + "\"");
+                }
+            });
+            text = "-baidu AND -csdn " + text;
+        }
+
+        // 解析搜索链接
+        var url = engine_now.url.replace("${word}", text);
+        if (config.debug) {
+            console.log("search [" + text + "] use [" + engine_now.title + "]; url=" + url);
+        }
+        window.open(url);
+    }
+}
+
+function trans() {
+    // 获取输入文本
+    var text = document.getElementsByClassName("input")[0].value;
+    if (text != "") {
+        // 解析翻译链接
+        var url = translate_engine_now.url.replace("${word}", text);
+        if (config.debug) {
+            console.log("translate [" + text + "] use [" + translate_engine_now.title + "]; url=" + url);
+        }
+        window.open(url);
+    }
+}
+
+/**
+ * 监听回车按键，用以回车搜索功能
+ */
+function searchEnter() {
+    // enter 键对应的 keyCode 为 13
+    if (window.event.keyCode == 13) {
+        search();
+    }
+}
+
+/**
+ * 监听Alt+Enter，用以翻译功能
+ */
+function transEnter(e) {
+    if ((e.altKey) && (window.event.keyCode == 13)) {
+        trans()
+        return true;
+    }
+}
+
+/**
+ * 新建一个tab，打开网站
+ * @param {*} div #div
+ */
+function goto(div) {
+    var node = div.getElementsByTagName("div")[0];
+    var url = node.attributes["url"].nodeValue;
+    if (config.debug) {
+        console.log("goto-website: " + url);
+    }
+    window.open(url);
+}
+
+function clearText() {
+    var input = document.getElementsByClassName("input")[0];
+    input.innerText = "";
+}
+
+function addWebsite(title, url, image) {
+    if (config.debug) {
+        console.log("add-website: " + title + ", " + url + ", " + image);
+    }
+    var website = document.getElementsByClassName("website")[0];
+
+    var li = document.createElement("li");
+
+    var div = document.createElement("div");
+    div.className = "site";
+    div.setAttribute("onclick", "goto(this)");
+
+    var img = document.createElement("img");
+    img.className = "image";
+    img.src = image;
+
+    var name = document.createElement("div");
+    name.className = "name";
+    name.setAttribute("url", url);
+    name.innerText = title;
+
+    website.appendChild(li);
+    li.appendChild(div);
+    div.appendChild(img);
+    div.appendChild(name);
+}
